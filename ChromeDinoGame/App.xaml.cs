fe54d03072a026d05327e037791512560f09b8bd
nastezii <nastiashalak@gmail.com>
2025-04-06 14:08:40 +0300

    Add project files.

---
+++ ChromeDinoGame/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+
+namespace ChromeDinoGame
+{
+    public partial class App : Application
+    {
+
+    }
+}
---
+++ ChromeDinoGame/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+
+namespace ChromeDinoGame
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+    }
+}

29067ff2714017c7a791a5e7d29c8afe3b4c0fe5
nastezii <nastiashalak@gmail.com>
2025-04-06 22:05:53 +0300

    implemented obstacle classes

---
+++ ChromeDinoGame/Entities/Obstacles/Bird.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace ChromeDinoGame.Entities.Obstacles
+{
+    class Bird : Obstacle
+    {
+        public Bird(double x, double y) : base(x, y) 
+        {
+            Sprite = new Image
+            {
+                Source = new BitmapImage(new Uri("pack://application:,,,/Resources/bird_fly.gif"))
+            };
+        } 
+    }
+}
---
+++ ChromeDinoGame/Entities/Obstacles/Cactus.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace ChromeDinoGame.Entities.Obstacles
+{
+    class Cactus : Obstacle
+    {
+        private static Random _random = new Random();
+
+        public Cactus(double x, double y) : base(x, y)
+        {
+            int num = _random.Next(1, 7);
+
+            Sprite = new Image
+            {
+                Source = new BitmapImage(new Uri($"pack://application:,,,/Resources/cactus{num}.png"))
+            };
+        }
+    }
+}

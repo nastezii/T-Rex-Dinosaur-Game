324670846318ee6c2a6cfb25472a6cd7cd966a10
nastezii <nastiashalak@gmail.com>
2025-04-09 21:46:57 +0300

    start implementing core game flow

---
+++ ChromeDinoGame/Services/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Controls;
+using System.Windows.Threading;
+using ChromeDinoGame.Entities.Obstacles;
+
+namespace ChromeDinoGame.Services
+{
+    class GameManager
+    {
+        private Canvas _canvas;
+        private DispatcherTimer _gameTimer;
+        private ObstaclesGenerator _obstaclesGenerator;
+
+        public GameManager(Canvas canvas) 
+        {
+            _canvas = canvas;
+        }
+    }
+}
---
+++ ChromeDinoGame/Services/ObjectHandler.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Windows.Controls;
+using ChromeDinoGame.Entities;
+
+namespace ChromeDinoGame.Services
+{
+    class ObjectHandler
+    {
+        private Canvas _canvas;
+        private List<GameEntity> _objects;
+        public ObjectHandler(Canvas canvas)
+        {
+            _canvas = canvas;
+            _objects = new List<GameEntity>();  
+        }
+
+        public void AddObject(GameEntity entity) => _objects.Add(entity);
+
+    }
+}

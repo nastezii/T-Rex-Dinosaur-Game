0c71d1bbe623fcc993c25e6ebf26c3e4878aba3f
nastezii <nastiashalak@gmail.com>
2025-04-09 16:02:17 +0300

    add Dinosaur entity

---
+++ ChromeDinoGame/Entities/Dinosaur.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Controls;
+using WpfAnimatedGif;
+using System.Windows.Media.Imaging;
+
+namespace ChromeDinoGame.Entities
+{
+    class Dino : GameEntity
+    {
+        private const string _rinningGifPath = "pack://application:,,,/Resources/dino_run.gif";
+        private const string _crouchingGifPath = "pack://application:,,,/Resources/dino_crouch.gif";
+        private const string _startImagePath = "pack://application:,,,/Resources/dino_start.png";
+        private const string _endImagePath = "pack://application:,,,/Resources/dino_dead.png";
+
+        public Dino(double lineOfGround)
+        {
+            PosY = lineOfGround;
+            PosX = 50;
+        }
+    }
+}

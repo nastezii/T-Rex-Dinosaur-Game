048e7a2a1d1d9dd0ba0281daace745803eedbe0f
nastezii <nastiashalak@gmail.com>
2025-04-09 14:50:37 +0300

    implement cloud and road elements

---
+++ ChromeDinoGame/Entities/Cloud.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace ChromeDinoGame.Entities
+{
+    class Cloud : MovableEntity
+    {
+        public Cloud(double x, double y, Random random)
+        {
+            BitmapImage bitmapImage = new BitmapImage(new Uri($"pack://application:,,,/Resources/road_{random.Next(1, 4)}.png"));
+
+            Sprite = new Image
+            {
+                Source = bitmapImage,
+            };
+
+            PosX = x;
+            PosY = y;
+        }
+    }
+}

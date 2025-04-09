c11485f5bef59cf2f544e54144980e3c370d3c7f
nastezii <nastiashalak@gmail.com>
2025-04-09 19:27:41 +0300

    edits

---
+++ ChromeDinoGame/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using ChromeDinoGame.Entities.Obstacles;
+
+namespace ChromeDinoGame
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            AddBirdToCanvas();
+        }
+
+        private void AddBirdToCanvas()
+        {
+            ObstaclesGenerator gen = new ObstaclesGenerator(600, 350, 31);
+            for (int i = 0; i < 3; i++)
+            {
+                Obstacle obj = gen.GenerateObstacle();
+                GameCanvas.Children.Add(obj.Sprite);
+
+                Canvas.SetLeft(obj.Sprite, obj.PosX);
+                Canvas.SetTop(obj.Sprite, obj.PosY);
+            }
+        }
+    }
+}

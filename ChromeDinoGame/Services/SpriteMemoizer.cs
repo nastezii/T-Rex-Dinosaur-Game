09c09080e0fcd694ed278f0dd0a77e182e702103
nastezii <nastiashalak@gmail.com>
2025-06-03 23:27:17 +0300

    bugs fixed

--- ChromeDinoGame/Services/SpriteMemoizer.cs
+++ ChromeDinoGame/Services/SpriteMemoizer.cs
@@ -6,30 +6,30 @@ namespace ChromeDinoGame.Services
 {
     public static class SpriteMemoizer
     {
-        private static Dictionary<(string path, bool isGif),
-            (Image image, double width, double height)> _cache = new();
+        private static readonly Dictionary<(string path, bool isGif), BitmapImage> _bitmapCache = new();
 
         public static (Image image, double width, double height) SetSpriteCharacteristics(string path, bool isGif)
         {
-            if (_cache.TryGetValue((path, isGif), out var cached))
+            if (!_bitmapCache.TryGetValue((path, isGif), out var bitmap))
             {
-                return cached;
+                bitmap = new BitmapImage();
+                bitmap.BeginInit();
+                bitmap.UriSource = new Uri(path, UriKind.RelativeOrAbsolute);
+                bitmap.CacheOption = BitmapCacheOption.OnLoad;
+                bitmap.EndInit();
+                bitmap.Freeze(); 
+
+                _bitmapCache[(path, isGif)] = bitmap;
             }
 
             var sprite = new Image();
-            var bitmap = new BitmapImage(new Uri(path, UriKind.RelativeOrAbsolute));
 
             if (isGif)
                 ImageBehavior.SetAnimatedSource(sprite, bitmap);
             else
                 sprite.Source = bitmap;
 
-            double width = bitmap.Width;
-            double height = bitmap.Height;
-
-            var result = (sprite, width, height);
-            _cache[(path, isGif)] = result;
-            return result;
+            return (sprite, bitmap.Width, bitmap.Height);
         }
     }
 }
